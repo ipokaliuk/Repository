33cc185de14a26320560ea15d4fc7e128f5dd42b
ipokaliuk <166396205+ipokaliuk@users.noreply.github.com>
2024-04-08 15:13:01 +0300

    Add files via upload

---
+++ prac11var6.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public abstract class Shape2D
+{
+    public abstract double Area();
+}
+
+public class Triangle : Shape2D
+{
+    public double Base { get; set; }
+    public double Height { get; set; }
+
+    public Triangle(double @base, double height)
+    {
+        Base = @base;
+        Height = height;
+    }
+
+    public override double Area()
+    {
+        return Base * Height / 2;
+    }
+}
+
+public class Trapezoid : Shape2D
+{
+    public double Base1 { get; set; }
+    public double Base2 { get; set; }
+    public double Height { get; set; }
+
+    public Trapezoid(double base1, double base2, double height)
+    {
+        Base1 = base1;
+        Base2 = base2;
+        Height = height;
+    }
+
+    public override double Area()
+    {
+        return (Base1 + Base2) * Height / 2;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var triangle = new Triangle(3, 4);
+        var trapezoid = new Trapezoid(3, 4, 5);
+
+        Console.WriteLine($"Площа трикутника: {triangle.Area()}");
+        Console.WriteLine($"Площа трапеції: {trapezoid.Area()}");
+    }
+}
